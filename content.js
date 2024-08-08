45eba0cbd87025d297b136e3c704b7c707c25cb5
gojodev <gojodev@users.noreply.github.com>
2024-08-08 01:51:49 +0100

    now checks if the user can even go forwards or backwards before showing animations, functional code

--- content.js
+++ content.js
@@ -1,46 +1,55 @@
-document.getElementById("leftArrow").style.opacity = 0;
-document.getElementById("rightArrow").style.opacity = 0;
+function hideArrows() {
+    document.getElementById("leftArrow").style.opacity = 0;
+    document.getElementById("rightArrow").style.opacity = 0;
+    updateLeftPos = 0;
+    updateRightPos = 0;
+}
+
+hideArrows();
+
+var TRIGGER_AMOUNT = 150; // the actual trigger amount
+var DISPLAY_AMOUNT = 125; // how far it goes on the screen
+var updateLeftPos = 0;
+var updateRightPos = 0;
+var inactivityTimeout;
+var moveID;
+var navAPI = typeof navigation === 'undefined';
 
 function showAnimation(amt, id) {
     let element = document.getElementById(id);
     element.style.transition = 'none';
     element.style.top = '50%';
     element.style.position = "absolute";
+    amt = Math.abs(amt);
 
     element.style.opacity = 1;
-    if (amt >= SCROLL_AMOUNT) {
-        if (id === "leftArrow" && history.length > 1) {
-            window.history.back();
+    if (amt >= DISPLAY_AMOUNT) {
+        if (id === "leftArrow") {
+            history.back();
         } else {
-            window.history.forward();
+            history.forward();
         }
     }
 }
 
-var SCROLL_AMOUNT = 200;
-
-var updateLeftPos = 0;
-var updateRightPos = 0;
-
 function translate(amt, id) {
-    if (id == "leftArrow") {
+    // ? compatibility concern for firefox - https://developer.mozilla.org/en-US/docs/Web/API/Navigation/canGoForward#browser_compatibility
+    if (id == "leftArrow" && updateLeftPos < DISPLAY_AMOUNT && (navAPI || navigation.canGoBack)) {
         updateLeftPos += amt;
         let left = document.getElementById(id);
         left.style.transform = `translate(${updateLeftPos}px)`;
         left.style.left = '0px';
 
-        // console.log("left: ", updateLeftPos);
-        showAnimation(Math.abs(updateLeftPos), id);
+        showAnimation(updateLeftPos, id);
     }
 
-    else {
+    else if (id == "rightArrow" && updateRightPos < DISPLAY_AMOUNT && (navAPI || navigation.canGoForward)) {
         updateRightPos += amt;
         let right = document.getElementById(id);
         right.style.transform = `translate(-${updateRightPos}px)`;
         right.style.right = '0px';
-        // console.log("right: ", updateRightPos);
 
-        showAnimation(Math.abs(updateRightPos), id);
+        showAnimation(updateRightPos, id);
     }
 }
 
@@ -48,14 +57,16 @@ function handleWheelEvent(e) {
     let deltaX = e.deltaX;
     if (deltaX !== 0) {
         if (deltaX < 0) {
+            moveID = 'left';
             translate(Math.abs(deltaX), "leftArrow");
 
         } else {
+            moveID = 'right';
             translate(Math.abs(deltaX), "rightArrow");
         }
 
-        // todo hide the arrows when the user isn't touching the mousepad
-        // semi working solution in test.js
+        clearTimeout(inactivityTimeout);
+        inactivityTimeout = setTimeout(hideArrows, 100);
     }
 }
 
