79df738072b2210ba6c171cc4712d990516e7a1b
gojodev <gojodev@users.noreply.github.com>
2025-01-26 03:09:47 +0000

    now using V3

--- background.js
+++ background.js
@@ -1,16 +1,21 @@
 console.log('Background script running...');
 
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(() => {
   console.log("Gesture Tab Navigator extension installed.");
+});
 
-  chrome.commands.onCommand.addListener(function (command) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      let tabId = tabs[0].id;
+chrome.commands.onCommand.addListener((command) => {
+  console.log(`Command received: ${command}`);
 
-      if (command === "forwardGesture") {
-        chrome.tabs.executeScript(tabId, { code: 'history.forward();' });
-      } else if (command === "backwardGesture") {
-        chrome.tabs.executeScript(tabId, { code: 'history.back();' });
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length === 0) return;
+
+    const tabId = tabs[0].id;
+    chrome.tabs.sendMessage(tabId, { action: command }, (response) => {
+      if (chrome.runtime.lastError) {
+        console.error("Error sending message to content script:", chrome.runtime.lastError);
+      } else {
+        console.log("Message sent to content script:", response);
       }
     });
   });
