4eac616faa220928ba48fb34991bfa0fafdf7362
gojodev <gojodev@users.noreply.github.com>
2025-03-19 15:41:41 +0000

    will only allow for navigation if the screen is NOT zoomed into TESTING NEEDED

--- content.js
+++ content.js
@@ -1,96 +1,96 @@
 console.log("SwipeBack content script loaded!");
 
 function createDiv() {
-    return document.createElement('div');
+  return document.createElement("div");
 }
 
 function create_arrowLeft() {
-    const arrowLeft = createDiv();
-    arrowLeft.classList.add('arrowLeft');
-    return arrowLeft;
+  const arrowLeft = createDiv();
+  arrowLeft.classList.add("arrowLeft");
+  return arrowLeft;
 }
 
 function create_arrowRight() {
-    const arrowRight = createDiv();
-    arrowRight.classList.add('arrowRight');
-    return arrowRight;
+  const arrowRight = createDiv();
+  arrowRight.classList.add("arrowRight");
+  return arrowRight;
 }
 
 function createArrows() {
-    const leftArrow = createDiv();
-    leftArrow.id = 'leftArrow';
+  const leftArrow = createDiv();
+  leftArrow.id = "leftArrow";
 
-    const leftContainer = createDiv();
-    leftContainer.classList.add('arrowContainer', 'leftPos');
-    leftArrow.appendChild(leftContainer);
+  const leftContainer = createDiv();
+  leftContainer.classList.add("arrowContainer", "leftPos");
+  leftArrow.appendChild(leftContainer);
 
-    const arrowSlidingLeft = createDiv();
-    arrowSlidingLeft.classList.add('arrowSlidingLeft');
-    arrowSlidingLeft.appendChild(create_arrowLeft());
-    leftContainer.appendChild(arrowSlidingLeft);
+  const arrowSlidingLeft = createDiv();
+  arrowSlidingLeft.classList.add("arrowSlidingLeft");
+  arrowSlidingLeft.appendChild(create_arrowLeft());
+  leftContainer.appendChild(arrowSlidingLeft);
 
-    const leftdelay1 = createDiv();
-    leftdelay1.classList.add('arrowSlidingLeft', 'delay1');
-    leftdelay1.appendChild(create_arrowLeft());
-    leftContainer.appendChild(leftdelay1);
+  const leftdelay1 = createDiv();
+  leftdelay1.classList.add("arrowSlidingLeft", "delay1");
+  leftdelay1.appendChild(create_arrowLeft());
+  leftContainer.appendChild(leftdelay1);
 
-    const leftdelay2 = createDiv();
-    leftdelay2.classList.add('arrowSlidingLeft', 'delay2');
-    leftdelay2.appendChild(create_arrowLeft());
-    leftContainer.appendChild(leftdelay2);
+  const leftdelay2 = createDiv();
+  leftdelay2.classList.add("arrowSlidingLeft", "delay2");
+  leftdelay2.appendChild(create_arrowLeft());
+  leftContainer.appendChild(leftdelay2);
 
-    const leftdelay3 = createDiv();
-    leftdelay3.classList.add('arrowSlidingLeft', 'delay3');
-    leftdelay3.appendChild(create_arrowLeft());
-    leftContainer.appendChild(leftdelay3);
-    document.body.append(leftArrow);
+  const leftdelay3 = createDiv();
+  leftdelay3.classList.add("arrowSlidingLeft", "delay3");
+  leftdelay3.appendChild(create_arrowLeft());
+  leftContainer.appendChild(leftdelay3);
+  document.body.append(leftArrow);
 
-    // console.log(leftArrow);
-    // ------------------------
+  // console.log(leftArrow);
+  // ------------------------
 
-    const rightArrow = createDiv();
-    rightArrow.id = 'rightArrow';
+  const rightArrow = createDiv();
+  rightArrow.id = "rightArrow";
 
-    const rightContainer = createDiv();
-    rightContainer.classList.add('arrowContainer', 'rightPos');
-    rightArrow.appendChild(rightContainer);
+  const rightContainer = createDiv();
+  rightContainer.classList.add("arrowContainer", "rightPos");
+  rightArrow.appendChild(rightContainer);
 
-    const arrowSlidingRight = createDiv();
-    arrowSlidingRight.classList.add('arrowSlidingRight');
-    arrowSlidingRight.append(create_arrowRight());
-    rightContainer.appendChild(arrowSlidingRight);
+  const arrowSlidingRight = createDiv();
+  arrowSlidingRight.classList.add("arrowSlidingRight");
+  arrowSlidingRight.append(create_arrowRight());
+  rightContainer.appendChild(arrowSlidingRight);
 
-    const rightdelay1 = createDiv();
-    rightdelay1.classList.add('arrowSlidingRight', 'delay1');
-    rightdelay1.appendChild(create_arrowRight());
-    rightContainer.appendChild(rightdelay1);
+  const rightdelay1 = createDiv();
+  rightdelay1.classList.add("arrowSlidingRight", "delay1");
+  rightdelay1.appendChild(create_arrowRight());
+  rightContainer.appendChild(rightdelay1);
 
-    const rightdelay2 = createDiv();
-    rightdelay2.classList.add('arrowSlidingRight', 'delay2');
-    rightdelay2.appendChild(create_arrowRight());
-    rightContainer.appendChild(rightdelay2);
+  const rightdelay2 = createDiv();
+  rightdelay2.classList.add("arrowSlidingRight", "delay2");
+  rightdelay2.appendChild(create_arrowRight());
+  rightContainer.appendChild(rightdelay2);
 
-    const rightdelay3 = createDiv();
-    rightdelay3.classList.add('arrowSlidingRight', 'delay3');
-    rightdelay3.appendChild(create_arrowRight());
-    rightContainer.appendChild(rightdelay3);
-    document.body.append(rightArrow);
+  const rightdelay3 = createDiv();
+  rightdelay3.classList.add("arrowSlidingRight", "delay3");
+  rightdelay3.appendChild(create_arrowRight());
+  rightContainer.appendChild(rightdelay3);
+  document.body.append(rightArrow);
 
-    // console.log(rightArrow);
+  // console.log(rightArrow);
 }
 
 createArrows();
 
 function hideArrows() {
-    document.getElementById("leftArrow").style.opacity = "0";
-    document.getElementById("rightArrow").style.opacity = "0";
-    updateLeftPos = 0;
-    updateRightPos = 0;
+  document.getElementById("leftArrow").style.opacity = "0";
+  document.getElementById("rightArrow").style.opacity = "0";
+  updateLeftPos = 0;
+  updateRightPos = 0;
 }
 
 hideArrows();
 
-document.body.addEventListener("onload", hideArrows)
+document.body.addEventListener("onload", hideArrows);
 
 const TRIGGER_AMOUNT = 150; // the actual trigger amount
 const DISPLAY_AMOUNT = 125; // how far it goes on the screen
@@ -98,77 +98,77 @@ var updateLeftPos = 0;
 var updateRightPos = 0;
 var inactivityTimeout;
 var moveID;
-var navAPI = typeof navigation === 'undefined';
+var navAPI = typeof navigation === "undefined";
 
 function hide(id) {
-    document.getElementById(id).style.opacity = 0;
+  document.getElementById(id).style.opacity = 0;
 }
 
 function show(id) {
-    document.getElementById(id).style.opacity = 1;
+  document.getElementById(id).style.opacity = 1;
 }
 
 function showAnimation(amt, id) {
-    let element = document.getElementById(id);
-    element.style.transition = 'none';
-    element.style.top = '50%';
-    element.style.position = "absolute";
+  let element = document.getElementById(id);
+  element.style.transition = "none";
+  element.style.top = "50%";
+  element.style.position = "absolute";
 
-    amt = Math.abs(amt);
+  amt = Math.abs(amt);
 
-    element.style.opacity = 1;
-    if (amt >= DISPLAY_AMOUNT) {
-        if (id === "leftArrow") {
-            hide(id);
-            history.back();
-        } else {
-            hide(id);
-            history.forward();
-        }
+  element.style.opacity = 1;
+
+  if (amt >= DISPLAY_AMOUNT && window.devicePixelRatio >= 1.25) {
+    if (id === "leftArrow") {
+      hide(id);
+      history.back();
+    } else {
+      hide(id);
+      history.forward();
     }
+  }
 }
 
 function translate(amt, id) {
-    const element = document.getElementById(id);
+  const element = document.getElementById(id);
 
-    if (id === "leftArrow" && updateLeftPos < DISPLAY_AMOUNT) {
-        show(id);
-        hide('rightArrow');
-        updateLeftPos += amt;
-        element.style.transform = `translate(${updateLeftPos}px)`;
-        element.style.left = '0px';
+  if (id === "leftArrow" && updateLeftPos < DISPLAY_AMOUNT) {
+    show(id);
+    hide("rightArrow");
+    updateLeftPos += amt;
+    element.style.transform = `translate(${updateLeftPos}px)`;
+    element.style.left = "0px";
 
-        showAnimation(updateLeftPos, id);
-    } else if (id === "rightArrow" && updateRightPos < DISPLAY_AMOUNT) {
-        show(id);
-        hide('leftArrow');
-        updateRightPos += amt;
-        element.style.transform = `translate(-${updateRightPos}px)`;
-        element.style.right = '0px';
+    showAnimation(updateLeftPos, id);
+  } else if (id === "rightArrow" && updateRightPos < DISPLAY_AMOUNT) {
+    show(id);
+    hide("leftArrow");
+    updateRightPos += amt;
+    element.style.transform = `translate(-${updateRightPos}px)`;
+    element.style.right = "0px";
 
-        showAnimation(updateRightPos, id);
-    }
+    showAnimation(updateRightPos, id);
+  }
 }
 
 function handleWheelEvent(e) {
-    let deltaX = e.deltaX;
-    if (deltaX !== 0) {
-        if (deltaX < 0) {
-            moveID = 'left';
-            translate(Math.abs(deltaX), "leftArrow");
-
-        } else {
-            moveID = 'right';
-            translate(Math.abs(deltaX), "rightArrow");
-        }
-
-        clearTimeout(inactivityTimeout);
-        inactivityTimeout = setTimeout(hideArrows, 100);
+  let deltaX = e.deltaX;
+  if (deltaX !== 0) {
+    if (deltaX < 0) {
+      moveID = "left";
+      translate(Math.abs(deltaX), "leftArrow");
+    } else {
+      moveID = "right";
+      translate(Math.abs(deltaX), "rightArrow");
     }
+
+    clearTimeout(inactivityTimeout);
+    inactivityTimeout = setTimeout(hideArrows, 100);
+  }
 }
 
-window.addEventListener('beforeunload', () => {
-    document.removeEventListener("mousewheel", handleWheelEvent);
+window.addEventListener("beforeunload", () => {
+  document.removeEventListener("mousewheel", handleWheelEvent);
 });
 
 document.addEventListener("wheel", handleWheelEvent);
